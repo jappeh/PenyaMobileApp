fe471550e7c9eb5f9e7fc20145c1c9054de0d4c9
jappeh <jasper.1995@live.be>
2017-04-19 17:02:16 +0200

    wp service

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -2,28 +2,25 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {NewsService} from "../../providers/wp.service";
 import {NewsDetail} from "../news-detail/news-detail";
 
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html',
+
+  providers:[NewsService]
 })
 export class NewsPage {
 
   url: string = 'http://www.fanatiek-fcbarcelona.be/wp-json/wp/v2/posts';
   items: any;
 
-  constructor(public navCtrl: NavController, private http:Http) {
+  constructor(public navCtrl: NavController, public newsService:NewsService) {
   }
 
   ionViewDidEnter() {
-    this.http.get( this.url )
-      .map(res => res.json())
-      .subscribe(data => {
-        // we've got back the raw data, now generate the core schedule data
-        // and save the data for later reference
-        this.items = data;
-      });
+    this.newsService.getNews().subscribe(data => this.items = data);
   }
 
   itemTapped(event, item) {
---
+++ src/providers/wp.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the NewsProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class NewsService {
+  items: any;
+  constructor(public http: Http) {
+
+  }
+  getNews(){
+    return this.http.get( 'http://www.fanatiek-fcbarcelona.be/wp-json/wp/v2/posts' )
+      .map(res => res.json());
+  }
+
+
+}
